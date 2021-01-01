be3e5d4942a3ba650fabd12d5ed37b2ee188323d
Jean Pierre Velez <vcjpierre@gmail.com>
2021-01-01 15:11:12 -0400

    Fix specified key was too long error

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,6 +23,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        Schema::defaultStringLength(191);
+        
          if ($this->app->environment('production')) {
             \URL::forceScheme('https');
         }
